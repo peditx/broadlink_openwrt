9d74130429c82d2e21a2f16d338214aada3459d5
PeDitX <pedram.ale@me.com>
2025-04-28 21:15:47 +0330

    Update discovery.lua

--- files/usr/lib/lua/broadlink/discovery.lua
+++ files/usr/lib/lua/broadlink/discovery.lua
@@ -1,8 +1,5 @@
--- files/usr/lib/lua/broadlink/discovery.lua
 local socket = require("socket")
 local bit = require("bit")
-local crypto = require("crypto")
-local json = require("luci.json")
 
 module("broadlink.discovery", package.seeall)
 
@@ -10,34 +7,44 @@ function discover_devices()
     local udp = socket.udp()
     udp:settimeout(2)
     udp:setsockname("*", 80)
-    udp:setoption('broadcast', true)
-    
-    local discovery_packet = string.char(
+    udp:setoption("broadcast", true)
+
+    local packet = string.char(
         0x5A, 0xA5, 0xAA, 0x55,
         0x5A, 0xA5, 0xAA, 0x55,
         0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00
     )
-    
-    udp:sendto(discovery_packet, "255.255.255.255", 80)
-    
+
+    udp:sendto(packet, "255.255.255.255", 80)
+
     local devices = {}
     local start_time = os.time()
-    
+
     while os.time() - start_time < 5 do
         local data, ip = udp:receivefrom()
-        if data then
+        if data and #data >= 0x40 then
             local dev = {
+                ip = ip,
                 mac = string.format("%02X:%02X:%02X:%02X:%02X:%02X",
                     data:byte(0x34), data:byte(0x35),
                     data:byte(0x36), data:byte(0x37),
                     data:byte(0x38), data:byte(0x39)),
-                ip = ip,
-                type = data:byte(0x40)
+                type = get_device_type(data:byte(0x40))
             }
             table.insert(devices, dev)
         end
     end
-    
+
     return devices
+end
+
+function get_device_type(type_code)
+    local types = {
+        [0x2712] = "RM4 Pro",
+        [0x2737] = "RM Mini",
+        [0x272a] = "SP2",
+        [0x753e] = "MP1"
+    }
+    return types[type_code] or "Unknown"
 end
