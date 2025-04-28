6ccc300aecd50e9cda7da495c73e9c127646ca1f
PeDitX <pedram.ale@me.com>
2025-04-28 20:19:09 +0330

    Update devices.lua

--- files/usr/lib/lua/luci/model/cbi/broadlink/devices.lua
+++ files/usr/lib/lua/luci/model/cbi/broadlink/devices.lua
@@ -9,8 +9,11 @@ s.addremove = true
 s.anonymous = false
 
 s:option(Value, "name", "Device Name")
-s:option(Value, "mac", "MAC Address"):matches("%x+:%x+:%x+:%x+:%x+:%x+")
-s:option(Value, "ip", "IP Address"):ipaddr()
+mac = s:option(Value, "mac", "MAC Address")
+mac.datatype = "macaddr"
+ip = s:option(Value, "ip", "IP Address")
+ip.datatype = "ip4addr"
+
 s:option(ListValue, "type", "Device Type")
 s:option(Flag, "enable", "Enabled")
 
