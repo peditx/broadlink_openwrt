11c3185ec08264dbb8769dd63cd4935dba137b35
PeDitX <pedram.ale@me.com>
2025-04-28 20:58:31 +0330

    Update devices.lua

--- files/usr/lib/lua/luci/model/cbi/broadlink/devices.lua
+++ files/usr/lib/lua/luci/model/cbi/broadlink/devices.lua
@@ -1,4 +1,3 @@
--- files/usr/lib/lua/luci/model/cbi/broadlink/devices.lua
 local uci = luci.model.uci.cursor()
 
 m = Map("broadlink", "Broadlink Devices")
@@ -9,12 +8,13 @@ s.addremove = true
 s.anonymous = false
 
 s:option(Value, "name", "Device Name")
-mac = s:option(Value, "mac", "MAC Address")
-mac.datatype = "macaddr"
-ip = s:option(Value, "ip", "IP Address")
-ip.datatype = "ip4addr"
-
-s:option(ListValue, "type", "Device Type")
+s:option(Value, "mac", "MAC Address"):matches("%x+:%x+:%x+:%x+:%x+:%x+")
+s:option(Value, "ip", "IP Address"):ipaddr()
+s:option(ListValue, "type", "Device Type") -- این خط باید مقادیر مجاز را تعریف کند
+s.option:value("RM4", "RM4 Pro")
+s.option:value("RM_Mini", "RM Mini")
+s.option:value("SP2", "Smart Plug SP2")
+s.option:value("MP1", "Multi Plug MP1")
 s:option(Flag, "enable", "Enabled")
 
 return m
